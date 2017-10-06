69f87e9d597142a54d9073b1ff90251b96f6bb02
Apoorv Rastogi <apoorvrastogi58@gmail.com>
2017-10-06 20:32:47 +0530

    all updated

--- app/src/main/java/com/example/asus/afinal/DonatorActivity.java
+++ app/src/main/java/com/example/asus/afinal/DonatorActivity.java
@@ -1,23 +1,32 @@
 package com.example.asus.afinal;
 
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.parse.FindCallback;
@@ -26,6 +35,7 @@ import com.parse.ParseGeoPoint;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import org.apache.http.conn.ConnectTimeoutException;
 
@@ -102,20 +112,123 @@ public class DonatorActivity extends FragmentActivity implements OnMapReadyCallb
                                             ArrayList<Marker> markers=new ArrayList<>();
                                             mMap.clear();
                                             markers.add(mMap.addMarker(new MarkerOptions().position(driverLocationLatLng).title("Driver Location")));
+                                            markers.add(mMap.addMarker(new MarkerOptions().position(requestLoactionLatLng).title("Your Location").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))));
+
+                                            LatLngBounds.Builder builder=new LatLngBounds.Builder();
+                                            for (Marker marker : markers){
+                                                builder.include(marker.getPosition());
+
+                                            }
+                                        LatLngBounds bounds=builder.build();
+                                            int padding =60;
+                                            CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds,padding);
+                                            mMap.animateCamera(cu);
+                                            callButton.setVisibility(View.VISIBLE);
+                                            handler.postDelayed(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                    checkForUpdates();
+                                                }
+                                            },2000);
+
 
                                         }
                                     }
                                 }
                             }
                         }
-                    })
+                    });
                 }
             }
-        })
+        });
 
 
 
     }
+
+    public void logout(View view)
+    {
+
+        ParseUser.logOut();
+        Intent intent= new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(intent);
+
+
+
+    }
+
+    public void call(View view){
+        Log.i("Info","Call for Pickup");
+        if(requestActive){
+            ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Request");
+            query.whereEqualTo("username",ParseUser.getCurrentUser().getUsername());
+            query.findInBackground(new FindCallback<ParseObject>() {
+                @Override
+                public void done(List<ParseObject> list, ParseException e) {
+                    if(e==null){
+                        if(objects.size()>0){
+                            for (ParseObject object : objects){
+                                object.deleteInBackground();
+                            }
+                        requestActive = false;
+                            callButton.setText("Call for Pickup");
+                        }
+
+                    }
+                }
+            });
+        }else{
+            if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+                Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if (lastKnownLocation!=null){
+                    ParseObject request=new ParseObject("Request");
+                    request.put("username",ParseUser.getCurrentUser().getUsername());
+                    ParseGeoPoint parseGeoPoint = new ParseGeoPoint(lastKnownLocation.getLatitude(),lastKnownLocation.getLongitude());
+                    request.put("location",parseGeoPoint);
+                    request.saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e==null){
+                                callButton.setText("Cancel pickup");
+                                requestActive=true;
+                                checkForUpdates();
+                            }
+                        }
+                    });
+                }else{
+                    Toast.makeText(this, "Could not find loaction. Please try again later", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions,grantResults);
+        if (requestCode ==1){
+            if(grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOACTION )== PackageManager.PERMISSION_GRANTED){
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+                    Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    updateMap(lastKnownLocation);
+                }
+            }
+        }
+    }
+
+    public void updateMap(Location location) {
+    if(driverActive==false){
+        LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.clear();
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,15));
+        mMap.addMarker(new MarkerOptions().position(userLocation).title("your Location"));
+    }
+
+
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -125,6 +238,28 @@ public class DonatorActivity extends FragmentActivity implements OnMapReadyCallb
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    callButton = (Button)findViewById(R.id.callButton);
+        infoTextView = (TextView)findViewById(R.id.infoTextView);
+        ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Request");
+        query.whereEqualTo("username",ParseUser.getCurrentUser().getUsername());
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> list, ParseException e) {
+                if(e==null){
+                    if(objects.size()>0){
+                        requestActive = true;
+                        callButton.setText("Cancel pickup");
+                        checkForUpdates();
+                    }
+                }
+            }
+        });
+
+
+
+
+
+
     }
 
 
@@ -140,10 +275,43 @@ public class DonatorActivity extends FragmentActivity implements OnMapReadyCallb
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        locationManager =(locationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                updateMap(location);
+            }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+        if(Build.VERSION.SDK_INT < 23){
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+
+        }else{
+            if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+
+            }else{
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+                Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if (lastKnownLocation!=null){
+                    updateMap(lastKnownLocation);
+                }
+            }
+        }
+
     }
 }
